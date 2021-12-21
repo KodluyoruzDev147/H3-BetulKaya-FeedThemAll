5b35340e5229f429ecac0c0e853935474ec2f5a3
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-12-21 17:04:00 +0300

    + Refactore : refactore player swerve movements

--- Feed Them All/Assets/Player/Scripts/PlayerController.cs
+++ Feed Them All/Assets/Player/Scripts/PlayerController.cs
@@ -11,19 +11,35 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform visual;
     [SerializeField] private Transform camPos;
 
-    [BoxGroup("Player Settings")]
-    [SerializeField] private float forwardSpeed;
-    [BoxGroup("Player Settings")]
-    [SerializeField] private float sideSpeed;
 
-    private Touch touch;
+    [BoxGroup("Player Input Settings")]
+    [SerializeField] private float forwardSpeed;
+    [BoxGroup("Player Input Settings")]
+    [SerializeField] private MobileSwerveInputs _mobileSwerveInputs;
+    [BoxGroup("Player Input Settings")]
+    [SerializeField] private EditorSwerveInputs _editorSwerveInputs;
+    [BoxGroup("Player Input Settings")]
+    [SerializeField] private float swerveSpeed = 0.5f;
+    [BoxGroup("Player Input Settings")]
+    [SerializeField] private float maxSwerveAmount = 1f;
+
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+#if UNITY_EDITOR
+        EditorMovement();
+#else
+        MobileMovement();
+#endif
+
+    }
+
+    #region Fixed Update
     void FixedUpdate()
     {
         // // Editor movements
@@ -31,16 +47,40 @@ public class PlayerController : MonoBehaviour
         // Vector3 sideMove = Vector3.right * Input.GetAxis("Horizontal") * sideSpeed * Time.fixedDeltaTime;
         // _rigidbody.MovePosition(transform.position + forwardMove + sideMove);
 
-        Vector3 deltaPosition = -visual.forward * forwardSpeed;
-        if (Input.touchCount > 0)
-        {
-            Vector3 touchPosition = Input.GetTouch(0).position;
-            if (touchPosition.x > Screen.width * 0.5f)
-                deltaPosition += visual.right * sideSpeed;
-            else
-                deltaPosition -= visual.right * sideSpeed;
-        }
-        visual.transform.position = visual.position + deltaPosition * Time.fixedDeltaTime;
+
+        // Vector3 deltaPosition = -visual.forward * forwardSpeed;
+        // if (Input.touchCount > 0)
+        // {
+        //     Vector3 touchPosition = Input.GetTouch(0).position;
+        //     if (touchPosition.x > Screen.width * 0.4f)
+        //         deltaPosition += visual.right * sideSpeed;
+        //     else
+        //         deltaPosition -= visual.right * sideSpeed;
+        // }
+        // visual.transform.position = visual.position + deltaPosition * Time.fixedDeltaTime;
+        // camPos.position = new Vector3(camPos.position.x, camPos.position.y, visual.transform.position.z);
+    }
+    #endregion
+
+    private void EditorMovement()
+    {
+        Vector3 forwardMove = Vector3.back * forwardSpeed * Time.deltaTime;
+        float swerveAmount = Time.deltaTime * swerveSpeed * _editorSwerveInputs.MoveFactorX;
+
+        swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
+        visual.transform.Translate(swerveAmount, 0, forwardMove.z);
         camPos.position = new Vector3(camPos.position.x, camPos.position.y, visual.transform.position.z);
+
+    }
+
+    private void MobileMovement()
+    {
+        Vector3 forwardMove = Vector3.back * forwardSpeed * Time.deltaTime;
+        float swerveAmount = Time.deltaTime * swerveSpeed * _mobileSwerveInputs.MoveFactorX;
+
+        swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
+        visual.transform.Translate(swerveAmount, 0, forwardMove.z);
+        camPos.position = new Vector3(camPos.position.x, camPos.position.y, visual.transform.position.z);
+
     }
 }
