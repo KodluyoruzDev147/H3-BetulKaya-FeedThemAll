ed7370e525c58d14054160ab8e902585e279838c
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-12-22 09:07:12 +0300

    + Refactore : refactore player movement

--- Feed Them All/Assets/Player/Scripts/PlayerController.cs
+++ Feed Them All/Assets/Player/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rigidbody;
 
     [BoxGroup("Visual Settings")]
-    [SerializeField] private Transform visual;
+    [SerializeField] private Transform player;
+    [BoxGroup("Visual Settings")]
     [SerializeField] private Transform camPos;
 
 
@@ -39,37 +40,14 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    #region Fixed Update
-    void FixedUpdate()
-    {
-        // // Editor movements
-        // Vector3 forwardMove = Vector3.back * forwardSpeed * Time.fixedDeltaTime;
-        // Vector3 sideMove = Vector3.right * Input.GetAxis("Horizontal") * sideSpeed * Time.fixedDeltaTime;
-        // _rigidbody.MovePosition(transform.position + forwardMove + sideMove);
-
-
-        // Vector3 deltaPosition = -visual.forward * forwardSpeed;
-        // if (Input.touchCount > 0)
-        // {
-        //     Vector3 touchPosition = Input.GetTouch(0).position;
-        //     if (touchPosition.x > Screen.width * 0.4f)
-        //         deltaPosition += visual.right * sideSpeed;
-        //     else
-        //         deltaPosition -= visual.right * sideSpeed;
-        // }
-        // visual.transform.position = visual.position + deltaPosition * Time.fixedDeltaTime;
-        // camPos.position = new Vector3(camPos.position.x, camPos.position.y, visual.transform.position.z);
-    }
-    #endregion
-
     private void EditorMovement()
     {
         Vector3 forwardMove = Vector3.back * forwardSpeed * Time.deltaTime;
         float swerveAmount = Time.deltaTime * swerveSpeed * _editorSwerveInputs.MoveFactorX;
 
         swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
-        visual.transform.Translate(swerveAmount, 0, forwardMove.z);
-        camPos.position = new Vector3(camPos.position.x, camPos.position.y, visual.transform.position.z);
+        transform.Translate(swerveAmount, 0, forwardMove.z);
+        camPos.position = new Vector3(camPos.position.x, camPos.position.y, player.transform.position.z);
 
     }
 
@@ -79,8 +57,8 @@ public class PlayerController : MonoBehaviour
         float swerveAmount = Time.deltaTime * swerveSpeed * _mobileSwerveInputs.MoveFactorX;
 
         swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
-        visual.transform.Translate(swerveAmount, 0, forwardMove.z);
-        camPos.position = new Vector3(camPos.position.x, camPos.position.y, visual.transform.position.z);
+        transform.Translate(swerveAmount, 0, forwardMove.z);
+        camPos.position = new Vector3(camPos.position.x, camPos.position.y, player.transform.position.z);
 
     }
 }
